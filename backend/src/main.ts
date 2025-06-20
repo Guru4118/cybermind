eee1b1f9e0aea4348a96c39b3042beda739597b5
Guru4118 <gprasath103@gmail.com>
2025-06-20 13:24:03 +0530

    frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
    app.enableCors({
-    origin: 'http://localhost:3000', // Allow frontend origin
+    origin: ['http://localhost:3000','https://cybermind1.onrender.com'], // Allow frontend origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
